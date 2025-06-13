fd786a6bf5837023b78c4498b3796964ec55dfb9
cecilia314 <lopezsisternacecilia@yahoo.com>
2025-06-13 15:31:52 -0700

    feat: Setup appwrite

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/Search';
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -45,7 +46,9 @@ function App() {
       }
       setMovieList(data.results || []);
 
-      console.log('Movies fetched successfully:', data.results);
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error('Error fetching movies:', error);
       setErrorMessage('Failed to fetch movies. Please try again later.');
